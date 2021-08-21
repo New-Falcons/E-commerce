2ed75070fb936722b62d71fc2a55f166eecca9fa
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-21 19:41:34 +0530

    random

--- src/components/WomenSection.jsx
+++ src/components/WomenSection.jsx
@@ -1,7 +1,29 @@
+import { women } from "../data/db.json";
+import { Link } from "react-router-dom";
+
 const WomenSection = () => {
   return (
-    <div>
-      <h1>Women Section</h1>
+    <div className="women">
+      <div className="main-Content">
+        {women.map((product) => (
+          <div className="inner CC">
+            <Link
+              to={`/product/${parseInt(product.id)}`}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <div className="content">
+                <img src={product.url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title" style={{ textAlign: "start" }}>
+                    {product.title}
+                  </h4>
+                  <h6 className="brand-name">Rs. {product.price}</h6>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
