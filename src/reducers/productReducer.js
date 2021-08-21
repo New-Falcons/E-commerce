5c3a879a27189869f73c05bff4db735522db3634
Rahul Mittal <rahul705691@gmail.com>
2021-08-21 14:34:45 +0530

    added cart page

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -519,6 +519,21 @@ const productReducer = (state = initState, action) => {
           (product) => parseInt(product.id) === parseInt(action.id)
         ),
       };
+    case "SEARCH":
+      // eslint-disable-next-line
+      const filterProducts = state.products.filter((product) => {
+        if (
+          product.description.toLowerCase().includes(action.payload) ||
+          product.title.toLowerCase().includes(action.payload)
+        ) {
+          return product;
+        }
+      });
+      console.log(filterProducts);
+      return {
+        ...state,
+        filteredProducts: filterProducts,
+      };
     default:
       return state;
   }
